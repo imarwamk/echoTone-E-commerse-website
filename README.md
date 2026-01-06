f6c6ff7829ef6d21394b76b36accae5cfe98ad01
imarwamk <marwa.alhrby7@gmail.com>
2026-01-06 13:52:29 +0300

    Add README with project images and assets

---
+++ README.md
@@ -0,0 +1,13 @@
+#
+
+<p align="center">
+  <img src="assets/sc/echoton2.png" width="250"/>
+  <img src="assets/sc/echoton3.png" width="250"/>
+  <img src="assets/sc/echoton4.png" width="250"/>
+</p>
+
+<p align="center">
+  <img src="assets/sc/echoton5.png" width="250"/>
+  <img src="assets/sc/echoton6.png" width="250"/>
+  <img src="assets/sc/echoton7.png" width="250"/>
+</p>
--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,12 @@
 <?php 
-$host ="localhost";
-$user ="root";
-$pass ="";
-$dbname = "products";
+$host = "sql212.infinityfree.com";      
+$user = "if0_40368981";                
+$pass = "imarwa1996joj";           
+$dbname = "if0_40368981_XXX";          
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-if($conn -> connect_error){
-    die("Connection failed:". $conn -> connect_error);
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
 } 
-
+?>
